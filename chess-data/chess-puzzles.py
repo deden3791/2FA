1803a68b6ee8831df8d99f175380faa160cbbcc7
ashleendaly <Ashleen.Daly@res-group.com>
2024-02-27 19:22:02 +0000

    ability to change chess puzzles difficulty and set timer on and off

--- chess-data/chess-puzzles.py
+++ chess-data/chess-puzzles.py
@@ -4,11 +4,19 @@ from tqdm import tqdm
 def load_puzzles():
     return pd.concat([chunk for chunk in tqdm(pd.read_csv("lichess_db_puzzle.csv.zst", chunksize=250000), desc='Loading puzzles', total=15)])
 
-def export_sample(df):
-    random_puzzles = df[["PuzzleId", "FEN", "Moves"]].sample(n=20)
-    random_puzzles.to_json('../my-app/src/data/chess-puzzles.json', orient='records')
+def export_sample(df, difficulty):
+    random_puzzles = df[["PuzzleId", "FEN", "Moves"]].sample(n=100)
+    random_puzzles.to_json(f'../my-app/src/data/chess-puzzles-{difficulty}.json', orient='records')
 
 if __name__ == "__main__":
     df = load_puzzles()
-    mate_df = df[df["Themes"].str.contains("mateIn1")]
-    export_sample(mate_df)
+
+    puzzle_themes = ((df["Themes"].str.contains("advantage")) & (df["Themes"].str.contains("oneMove"))) | (df["Themes"].str.contains("mateIn1"))
+    
+    easy_df = df[puzzle_themes & (df["Rating"] < 1000)]
+    medium_df = df[puzzle_themes & (df["Rating"] >= 1000) & (df["Rating"] < 2000)]
+    hard_df = df[puzzle_themes & (df["Rating"] >= 2000)]
+    
+    export_sample(easy_df, "easy")
+    export_sample(medium_df, "medium")
+    export_sample(hard_df, "hard")
--- my-app/src/components/ChessPuzzle.tsx
+++ my-app/src/components/ChessPuzzle.tsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import { Chess, Move } from "chess.js";
 import { Chessboard } from "react-chessboard";
-import puzzles from "../data/chess-puzzles.json";
+import easyPuzzles from "../data/chess-puzzles-easy.json";
+import mediumPuzzles from "../data/chess-puzzles-medium.json";
+import hardPuzzles from "../data/chess-puzzles-hard.json";
 import { useNavigate } from "react-router-dom";
 import Timer from "./Timer";
 
@@ -11,25 +13,62 @@ interface puzzle {
   Moves: string[];
 }
 
+type levels = "EASY" | "MEDIUM" | "HARD";
+
+// --------- SET LEVEL HERE
+
+const level: levels = "HARD";
+
+// --------- SET TIMER ON OR OFF HERE
+
+const timer = false;
+
+// ---------
+
 export const ChessPuzzle = () => {
   const navigate = useNavigate();
+
+  const [resetFlag, setResetFlag] = useState<boolean>(false);
+
   const [puzzle, setPuzzle] = useState<puzzle | null>(null);
   const [game, setGame] = useState(new Chess());
   const [attempts, setAttempts] = useState(0);
+  const [colourToMove, setColourToMove] = useState<string>("");
 
   useEffect(() => {
-    if (attempts > 3) navigate("/unauthenticated");
-    getRandomPuzzle();
+    if (attempts >= 3) navigate("/unauthenticated");
+    getRandomPuzzle(level);
   }, [attempts, navigate]);
 
-  const getRandomPuzzle = () => {
+  const getRandomPuzzle = (pickedLevel: levels) => {
+    const puzzles =
+      pickedLevel === "EASY"
+        ? easyPuzzles
+        : pickedLevel === "MEDIUM"
+        ? mediumPuzzles
+        : hardPuzzles;
+
     let chosenPuzzle = puzzles[Math.floor(Math.random() * puzzles.length)];
     let puzzleMoves = chosenPuzzle.Moves.split(" ");
     const gameInstance = new Chess(chosenPuzzle.FEN);
-    gameInstance.move(puzzleMoves[0]);
     setGame(gameInstance);
     setPuzzle({ ...chosenPuzzle, Moves: puzzleMoves });
+    setColourToMove("");
   };
+
+  useEffect(() => {
+    if (puzzle) {
+      const firstMove = {
+        from: puzzle.Moves[0].slice(0, 2),
+        to: puzzle.Moves[0].slice(2, 4),
+      };
+      setTimeout(() => {
+        const movedPiece = makeAMove(firstMove);
+        movedPiece.color === "b" ? setColourToMove("w") : setColourToMove("b");
+      }, 600);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [puzzle]);
 
   const makeAMove = (move: any): Move => {
     const gameCopy: Chess = new Chess(game.fen());
@@ -52,13 +91,20 @@ export const ChessPuzzle = () => {
       console.log(err);
     } finally {
       setAttempts((prev) => prev + 1);
+      setResetFlag((prev) => !prev);
       return false;
     }
   };
 
   return (
     <div className="passwordScreen">
-      {/* CSS on chessboard is not working, it is not centering */}
+      {colourToMove.length > 0 && colourToMove === "w" ? (
+        <label>White to Move</label>
+      ) : colourToMove === "b" ? (
+        <label>Black to Move</label>
+      ) : (
+        <label> </label>
+      )}
       <div>
         <Chessboard
           boardWidth={600}
@@ -66,7 +112,13 @@ export const ChessPuzzle = () => {
           onPieceDrop={onDrop}
         />
       </div>
-      <Timer stopFunction={() => setAttempts((prev) => prev + 1)} />
+
+      {timer && (
+        <Timer
+          stopFunction={() => setAttempts((prev) => prev + 1)}
+          resetFlag={resetFlag}
+        />
+      )}
     </div>
   );
 };
