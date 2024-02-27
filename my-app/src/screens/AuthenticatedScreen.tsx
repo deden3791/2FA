ee2a4aa552acbcdf6cff1c624e8a59d486857be8
LewisTrundle <lewis.trundle.1510@gmail.com>
2024-02-27 17:25:58 +0000

    added back to homepage button

--- my-app/src/screens/AuthenticatedScreen.tsx
+++ my-app/src/screens/AuthenticatedScreen.tsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 export const AuthenticatedScreen = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="passwordScreen">
       <label>Welcome back!</label>
+      <button onClick={() => navigate('/')}>Go back to home screen</button>
     </div>
   );
 };
